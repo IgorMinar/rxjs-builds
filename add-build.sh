7a0e0c7668866fa7f3ef6ccfdbb88a089622bc00
Igor Minar <igor@angular.io>
2018-03-07 21:18:14 -0800

    temporarily disable git rm

--- add-build.sh
+++ add-build.sh
@@ -39,7 +39,7 @@ cd ../rxjs
 cd -
   
   shopt -s extglob
-  git rm -rf --quiet !(LICENSE|add-build.sh)
+  # git rm -rf --quiet !(LICENSE|add-build.sh)
   mv ../rxjs/dist/package/* .
   git commit --all -m "rxjs build $BUILD_SCM_VERSION"
   git tag $BUILD_SCM_VERSION
